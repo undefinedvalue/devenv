068791e53dd0737c3caa307e3d1c1da776f2daa9
Anna Bell <undefinedvalue@users.noreply.github.com>
2014-10-02 14:40:16 -0700

    Fix issue with pathogen

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 SCRIPT_SOURCE=$(readlink -f ${0%/*})
+pushd $SCRIPT_SOURCE
 
 # Pull in the submodules in dependencies
 git submodule init
 git submodule update
+
+# One of my hosts hangs when loading the most recent commit of pathogen
+# Use v2.3, which seems stable
+pushd dependencies/vim-pathogen
+git checkout tags/v2.3
+popd
 
 ln -s -t "$HOME" "$SCRIPT_SOURCE/.vimrc" "$SCRIPT_SOURCE/.zshrc" "$SCRIPT_SOURCE/.gitconfig" "$SCRIPT_SOURCE/.tmux.conf"
 
@@ -15,3 +22,5 @@ ln -s -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-rails"
 ln -s -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-haml"
 ln -s -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/screen"
 ln -s -t "$HOME/.vim/colors" "$SCRIPT_SOURCE/.vim/colors/"*
+
+popd
