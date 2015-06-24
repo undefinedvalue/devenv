3b405c89ef524f59d1f3c451ad747b1e171a4c57
Anna Bell <undefinedvalue@users.noreply.github.com>
2015-06-24 00:14:16 +0000

    Silencing some ignorable error messages

--- setup.sh
+++ setup.sh
@@ -28,22 +28,22 @@ popd
 
 $ln_cmd -s -t "$HOME" "$SCRIPT_SOURCE/.vimrc" "$SCRIPT_SOURCE/.zshrc" "$SCRIPT_SOURCE/.gitconfig" "$SCRIPT_SOURCE/.tmux.conf"
 
-mkdir "$HOME/.vim"
-mkdir "$HOME/.vim/autoload"
-mkdir "$HOME/.vim/bundle"
-mkdir "$HOME/.vim/colors"
-$ln_cmd -s -t "$HOME/.vim/autoload" "$SCRIPT_SOURCE/dependencies/vim-pathogen/autoload/"*
-$ln_cmd -s -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-rails"
-$ln_cmd -s -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-haml"
-$ln_cmd -s -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-coffee-script"
-$ln_cmd -s -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-slim"
-$ln_cmd -s -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/screen"
-$ln_cmd -s -t "$HOME/.vim/colors" "$SCRIPT_SOURCE/.vim/colors/"*
+mkdir "$HOME/.vim" > /dev/null 2>&1
+mkdir "$HOME/.vim/autoload" > /dev/null 2>&1
+mkdir "$HOME/.vim/bundle" > /dev/null 2>&1
+mkdir "$HOME/.vim/colors" > /dev/null 2>&1
+$ln_cmd -s -f -t "$HOME/.vim/autoload" "$SCRIPT_SOURCE/dependencies/vim-pathogen/autoload/"*
+$ln_cmd -s -f -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-rails"
+$ln_cmd -s -f -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-haml"
+$ln_cmd -s -f -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-coffee-script"
+$ln_cmd -s -f -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/vim-slim"
+$ln_cmd -s -f -t "$HOME/.vim/bundle" "$SCRIPT_SOURCE/dependencies/screen"
+$ln_cmd -s -f -t "$HOME/.vim/colors" "$SCRIPT_SOURCE/.vim/colors/"*
 
 # Set up ruby
-mkdir "$HOME/.rbenv"
-$ln_cmd -s -t "$HOME/.rbenv" "$SCRIPT_SOURCE/dependencies/rbenv/"*
-mkdir "$HOME/.rbenv/plugins"
-$ln_cmd -s -t "$HOME/.rbenv/plugins" "$SCRIPT_SOURCE/dependencies/ruby-build"
+mkdir "$HOME/.rbenv" > /dev/null 2>&1
+$ln_cmd -s -f -t "$HOME/.rbenv" "$SCRIPT_SOURCE/dependencies/rbenv/"*
+mkdir "$HOME/.rbenv/plugins" > /dev/null 2>&1
+$ln_cmd -s -f -t "$HOME/.rbenv/plugins" "$SCRIPT_SOURCE/dependencies/ruby-build"
 
 popd
